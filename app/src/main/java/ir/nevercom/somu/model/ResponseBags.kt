571f1a3e4b92c1c2b69f2e16b55e7b291d1c75af
nevercom <nevercom@gmail.com>
2021-08-05 17:17:24 +0430

    Removed `Movie` model and corresponding API endpoints, as we are now using TMDB APIs

--- app/src/main/java/ir/nevercom/somu/model/ResponseBags.kt
+++ app/src/main/java/ir/nevercom/somu/model/ResponseBags.kt
@@ -6,10 +6,3 @@ data class LoginResponse(
     val token: String,
     val user: User
 )
-
-data class SearchResponse(
-    val page: Int,
-    @SerializedName("total_pages") val totalPages: Int,
-    @SerializedName("total_results") val totalResults: Int,
-    val results: List<Movie>
-)
--- app/src/main/java/ir/nevercom/somu/repositories/Api.kt
+++ app/src/main/java/ir/nevercom/somu/repositories/Api.kt
@@ -3,21 +3,11 @@ package ir.nevercom.somu.repositories
 import com.haroldadmin.cnradapter.NetworkResponse
 import ir.nevercom.somu.model.ErrorResponse
 import ir.nevercom.somu.model.LoginResponse
-import ir.nevercom.somu.model.Movie
-import ir.nevercom.somu.model.SearchResponse
 import ir.nevercom.somu.model.body.Login
-import retrofit2.http.*
+import retrofit2.http.Body
+import retrofit2.http.POST
 
 interface Api {
     @POST("login")
     suspend fun login(@Body request: Login): NetworkResponse<LoginResponse, ErrorResponse>
-
-    @GET("search")
-    suspend fun search(
-        @Query("q") query: String,
-        @Query("page") page: Int = 1
-    ): NetworkResponse<SearchResponse, ErrorResponse>
-
-    @GET("movie/{id}")
-    suspend fun getMovieById(@Path("id") id: Int): NetworkResponse<Movie, ErrorResponse>
 }
