2fdc1368f565a28a9a0d5d8b313f51d82a9e03c2
nevercom <nevercom@gmail.com>
2021-08-18 14:37:07 +0430

    disable ripple effect

--- app/src/main/java/ir/nevercom/somu/ui/component/ExpandingText.kt
+++ app/src/main/java/ir/nevercom/somu/ui/component/ExpandingText.kt
@@ -17,12 +17,21 @@
 package ir.nevercom.somu.ui.component
 
 import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.spring
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreHoriz
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
@@ -40,23 +49,52 @@ fun ExpandingText(
     var canTextExpand by remember(text) { mutableStateOf(true) }
     var expanded by remember { mutableStateOf(false) }
 
-    Text(
-        text = text,
-        style = textStyle,
-        textAlign = textAlign,
-        overflow = TextOverflow.Ellipsis,
-        maxLines = if (expanded) expandedMaxLines else collapsedMaxLines,
-        modifier = Modifier
-            .clickable(
-                enabled = expandable && canTextExpand,
-                onClick = { expanded = !expanded }
-            )
-            .animateContentSize(animationSpec = spring())
-            .then(modifier),
-        onTextLayout = {
-            if (!expanded) {
-                canTextExpand = it.hasVisualOverflow
+    Column {
+        Text(
+            text = text,
+            style = textStyle,
+            textAlign = textAlign,
+            overflow = TextOverflow.Ellipsis,
+            maxLines = if (expanded) expandedMaxLines else collapsedMaxLines,
+            modifier = Modifier
+                .noRippleClickable(
+                    enabled = expandable && canTextExpand,
+                    onClick = { expanded = !expanded }
+                )
+                .animateContentSize(animationSpec = spring(dampingRatio = Spring.DampingRatioLowBouncy))
+                .then(modifier),
+            onTextLayout = {
+                if (!expanded) {
+                    canTextExpand = it.hasVisualOverflow
+                }
             }
+        )
+        if (!expanded) {
+            Icon(
+                imageVector = Icons.Filled.MoreHoriz,
+                contentDescription = "Click to expand or collapse",
+                modifier = Modifier.align(CenterHorizontally)
+            )
         }
-    )
+    }
+}
+
+/**
+ * Taken from: [https://stackoverflow.com/a/66839858/1686304]
+ */
+inline fun Modifier.noRippleClickable(
+    enabled: Boolean = true,
+    onClickLabel: String? = null,
+    role: Role? = null,
+    crossinline onClick: () -> Unit
+): Modifier = composed {
+    clickable(
+        indication = null,
+        interactionSource = remember { MutableInteractionSource() },
+        enabled = enabled,
+        onClickLabel = onClickLabel,
+        role = role,
+    ) {
+        onClick()
+    }
 }
