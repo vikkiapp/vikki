50e53322f765584586fe2f794e4b960365c3c4b2
nevercom <nevercom@gmail.com>
2021-08-25 20:10:45 +0430

    since backend code is not implemented yet, go with mocked API for now

--- app/src/main/java/ir/nevercom/somu/repositories/Api.kt
+++ app/src/main/java/ir/nevercom/somu/repositories/Api.kt
@@ -5,9 +5,11 @@ import ir.nevercom.somu.model.ErrorResponse
 import ir.nevercom.somu.model.LoginResponse
 import ir.nevercom.somu.model.body.Login
 import retrofit2.http.Body
+import retrofit2.http.Headers
 import retrofit2.http.POST
 
 interface Api {
     @POST("login")
+    @Headers("Prefer: code=200, example=Successful Login")
     suspend fun login(@Body request: Login): NetworkResponse<LoginResponse, ErrorResponse>
 }
