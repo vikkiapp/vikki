9d602314e81bdc7d0ec820afb208f028399b5930
nevercom <nevercom@gmail.com>
2021-09-04 21:03:56 +0430

    create extension functions to add support for `LazyPagingItems` to `LazyVerticalGrid`

--- app/src/main/java/ir/nevercom/somu/util/Extensions.kt
+++ app/src/main/java/ir/nevercom/somu/util/Extensions.kt
@@ -1,11 +1,16 @@
 package ir.nevercom.somu.util
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.lazy.LazyGridScope
+import androidx.compose.foundation.lazy.LazyItemScope
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
 import androidx.compose.ui.semantics.Role
+import androidx.paging.compose.LazyPagingItems
 import de.vkay.api.tmdb.enumerations.MediaType
 import de.vkay.api.tmdb.enumerations.MediaType.*
 import de.vkay.api.tmdb.models.TmdbPerson
@@ -51,4 +56,24 @@ fun MediaType.toReadableString() = when (this) {
     SEASON -> "Seasons"
     EPISODE -> "Episodes"
     UNKNOWN -> "Unknown"
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+public fun <T : Any> LazyGridScope.items(
+    items: LazyPagingItems<T>,
+    itemContent: @Composable LazyItemScope.(value: T?) -> Unit
+) {
+    items(count = items.itemCount) { index ->
+        itemContent(items[index])
+    }
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+public fun <T : Any> LazyGridScope.itemsIndexed(
+    items: LazyPagingItems<T>,
+    itemContent: @Composable LazyItemScope.(index: Int, value: T?) -> Unit
+) {
+    items(items.itemCount) { index ->
+        itemContent(index, items[index])
+    }
 }
