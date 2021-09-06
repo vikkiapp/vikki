dd007261b252b0ac62b9de20d4d51cbf9bc57383
nevercom <nevercom@gmail.com>
2021-09-06 16:01:15 +0430

    completely switch to PagingData as View State

--- app/src/main/java/ir/nevercom/somu/ui/screen/discover/DiscoverScreenViewModel.kt
+++ app/src/main/java/ir/nevercom/somu/ui/screen/discover/DiscoverScreenViewModel.kt
@@ -1,57 +1,36 @@
 package ir.nevercom.somu.ui.screen.discover
 
-import androidx.lifecycle.*
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
-import com.haroldadmin.cnradapter.NetworkResponse
+import androidx.paging.cachedIn
 import dagger.hilt.android.lifecycle.HiltViewModel
 import de.vkay.api.tmdb.Discover
 import de.vkay.api.tmdb.TMDb
 import de.vkay.api.tmdb.models.TmdbMovie
-import de.vkay.api.tmdb.models.TmdbPage
-import de.vkay.api.tmdb.models.TmdbShow
-import ir.nevercom.somu.util.ViewState
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class DiscoverScreenViewModel @Inject constructor(
-    private val handle: SavedStateHandle,
+    handle: SavedStateHandle,
     private val tmdb: TMDb
 ) : ViewModel() {
     val title: String = handle.get("title") ?: "Discover Movies"
-    private val discoverSource = DiscoverSource(tmdb, DiscoverOptions.options)
-    val movies: Flow<PagingData<TmdbMovie.Slim>> = Pager(PagingConfig(pageSize = 20)) {
-        discoverSource
-    }.flow
+    private lateinit var _movies: Flow<PagingData<TmdbMovie.Slim>>
+    val movies get() = _movies
 
-
-    private val _state: MutableLiveData<DiscoverScreenViewState> = MutableLiveData(
-        DiscoverScreenViewState.Empty
-    )
-    val state: LiveData<DiscoverScreenViewState> = _state
-    private fun currentState(): DiscoverScreenViewState = _state.value!!
-
-    fun refresh(options: Discover.MovieBuilder) = viewModelScope.launch {
-        _state.value = currentState().copy(movies = ViewState.Loading())
-        when (val response = tmdb.discoverService.movie(options)) {
-            is NetworkResponse.Success -> {
-                _state.value = currentState().copy(movies = ViewState.Loaded(response.body))
-            }
-        }
+    init {
+        refresh(DiscoverOptions.options)
     }
-}
 
-data class DiscoverScreenViewState(
-    val movies: ViewState<TmdbPage<TmdbMovie.Slim>>,
-    val shows: ViewState<TmdbPage<TmdbShow.Slim>>,
-) {
-    companion object {
-        val Empty = DiscoverScreenViewState(
-            ViewState.Empty(),
-            ViewState.Empty(),
-        )
+    fun refresh(options: Discover.MovieBuilder?) = viewModelScope.launch {
+        _movies = Pager(PagingConfig(pageSize = 20)) { DiscoverSource(tmdb, options) }
+            .flow
+            .cachedIn(viewModelScope)
     }
 }
